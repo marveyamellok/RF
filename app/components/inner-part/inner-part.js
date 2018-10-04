a9cc8b33c6c06101770c3989682bce4c6e908220
m.kuznecova <m.kuznecova@peppers-studio.ru>
2018-10-04 16:14:14 +0300

    - правки для смены языка на всей странице при смене оного в JSON

--- app/components/inner-part/inner-part.js
+++ app/components/inner-part/inner-part.js
@@ -1,34 +1,40 @@
 $(function () {
-  // var data;
+  var data;
 
-  // $(window).on( "main:ready", function( e, _data ) {
-  //   data = _data;
-  //   showContent();
-  // });
+  $(window).on( "main:ready", function( e, _data ) {
+    data = _data;
+    showContent();
+  });
 
-  // $(window).on("main:page_changed", function( e, _data){
-  //   data = _data;
-  //   hideContent();
-  // });
+  $(window).on("main:page_changed", function( e, _data){
 
-  // function hideContent(){
-  //   $('.inner-part').fadeOut( "fast", function() {
-  //     showContent()
-  //   });
-  // }
+    data = _data;
+    hideContent( showContent );
 
-  // function showContent(){
-  //   var pageNow = data.header.current_page;
-  //   var lang = data.header.current_language;
-  //   var context = data.content[pageNow];
-  //   var theTemplateScript = $("#entry-template").html();
-  //   var theTemplate = Handlebars.compile(theTemplateScript);
-  //   var theCompiledHtml = theTemplate( { image: context.image, title: context.title[lang], text:context.text[lang] } );
+  });
 
-  //   $('.inner-part')
-  //     .html(theCompiledHtml)
-  //     .fadeIn( "slow" )
-  //   ;  
-  // }
+  function hideContent(m){
+
+    $('.inner-part').fadeOut( "fast", function() {
+      if( m ) m();
+    });
+
+  }
+
+  function showContent(){
+
+    var pageNow = data.header.current_page;
+    var lang = data.header.current_language;
+    var context = data.content[pageNow];
+    var theTemplateScript = $("#entry-template").html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    var theCompiledHtml = theTemplate( { image: context.image, title: context.title[lang], text:context.text[lang] } );
+
+    $('.inner-part')
+      .html(theCompiledHtml)
+      .fadeIn( "slow" )
+    ;
+
+  }
 
 });
--- app/components/menu-inner/menu-inner.js
+++ app/components/menu-inner/menu-inner.js
@@ -7,62 +7,89 @@ $(function(){
 
   $(window).on( "main:ready", function( e, _data ) {
     
+    //
     data = _data;
+    var lang;
+    getCurrentLanguage();
     var $menu_items = $(".menu-inner__items", $element );
+    var $elements = [];
+
 
     data.menu.list.forEach(function(item,index){
 
       if (item.text){
-        content = item.text.ru;
+        content = item.text;
         page = item.page;
       }
 
-      addContent(content, item, page);
+      addContent( content, item, page );
 
       $menu_item = $(".menu-inner__item", $element );
       $($menu_item[0]).addClass("menu-inner__item_choosed");
 
     });
 
-    $($menu_item).on('click', function(){
-      $($menu_item).removeClass("menu-inner__item_choosed");
-      var $this = $(this);
-      $this.addClass("menu-inner__item_choosed");
-      page = $this.data("page");
-      data.header.current_page = page;
 
-      $(window).trigger( "main:page_changed", data);
+
+    //
+    $($menu_item).on('click', function(){
+
+      $($menu_item).removeClass("menu-inner__item_choosed");
+      $(this).addClass("menu-inner__item_choosed");
+      
+      page = $(this).data("page");
+
+      $(window).trigger( "menu:change_page", page );
+
     });
 
-    // $(window).on("main:page_changed", function(){
-    //   changeContent();
-    // })
+    $(window).on("main:page_changed", function(){
+      
+      changeContent();
 
-    function addContent(content, item, page){
-      if( item.type == 'separator' ){
-        var $item = $('<li class="menu-inner__item menu-inner__item-line"></li>' ).appendTo( $menu_items );
-      }else{
-        var $item = $('<li class="menu-inner__item" data-page="' + page +  '"><a href="#" data-use-city="true" class="menu-inner__item-link">'+ content + '</a></li>' ).appendTo( $menu_items );
-      }
+    });
+
+
+
+    //
+    function changeContent(){
+
+      getCurrentLanguage();
+
+      $elements.forEach(function(e,i){
+        var $e = $(e);
+        $e.html( $e.data( "content" )[ lang ] );
+      });
+
     }
 
-    // function changeContent(){
-    //   var lang = data.header.current_language;
-    //   var content = [];
 
-    //   data.menu.list.forEach(function(item, index){
-    //     if (item.text){
-    //       var elem = item.text[lang]
-    //       content.push(elem)
-    //     }
-    //   })
+    //
+    function addContent(content, item, page){
 
-    //   $.each($(".menu-inner__item-link"), function(index, item ){
-    //     $(item).html("")
-    //     $(item).html(content[index])
-    //   });
-    // }
+      switch( item.type ){
 
+        case 'separator': 
+          var $item = $('<li class="menu-inner__item menu-inner__item-line"></li>' ).appendTo( $menu_items );
+          break;
+
+        default:
+          var $item =
+            $('<li class="menu-inner__item" data-page="' + page +  '"><a href="#" data-use-city="true" class="menu-inner__item-link">'+ content[lang] + '</a></li>' )
+              .appendTo( $menu_items )
+              .data('content', content )
+            ;
+          $elements.push( $item );
+          break;
+
+      }
+
+    }
+
+    //
+    function getCurrentLanguage() {
+      lang = data.header.current_language;
+    }
 
   });
 
