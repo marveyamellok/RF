23bfcdd0a423df2800cf2e14464c1216d91aaa19
m.kuznecova <m.kuznecova@peppers-studio.ru>
2018-10-04 19:01:21 +0300

    - анимация

--- app/components/inner-part/inner-part.js
+++ app/components/inner-part/inner-part.js
@@ -1,5 +1,6 @@
 $(function () {
   var data;
+  var $height;
 
   $(window).on( "main:ready", function( e, _data ) {
     data = _data;
@@ -13,12 +14,27 @@ $(function () {
 
   });
 
-  function hideContent(m){
+  function hideContent(tweenComplete){
 
-    $('.inner-part').fadeOut( "fast", function() {
-      if( m ) m();
-    });
+    var $elem = $('.inner-part');
+    // var $height = $('.inner-part-content').height();
 
+    // TweenMax.fromTo($elem, 1, {css: {height: $height}}, {css:{height:"0"}});
+
+    // if( onComplete ) onComplete();
+
+    // var tl = new TimelineLite();
+
+    // tl.from($(".inner-part__title"), 0.1, {css: {opacity: 1}})
+    //   .from($(".inner-part-column"), 0.1, {y:  0, opacity: 1})
+    //   .to($(".inner-part__title"), 1, {css: {opacity: 0}})
+    //   .to($(".inner-part-column"), 1, {y: 100, opacity: 0})
+
+    var title = TweenMax.fromTo($(".inner-part__title"), 1, {opacity: 1, x: 0, ease: Power3.easeOut}, {opacity: 0, x: 150, ease: Power3.easeOut});
+    var text = TweenMax.fromTo($(".inner-part-column"), 1, {y:  0, opacity: 1}, {y: 100, opacity: 0, onComplete:tweenComplete} );
+    var image =  TweenMax.fromTo($(".inner-part__image"), 1.5, {rotation: 0, opacity: 1, scale: 1}, {rotation: 720, opacity: 0, scale: 0});
+
+    text.delay(0.5);
   }
 
   function showContent(){
@@ -30,11 +46,10 @@ $(function () {
     var theTemplate = Handlebars.compile(theTemplateScript);
     var theCompiledHtml = theTemplate( { image: context.image, title: context.title[lang], text:context.text[lang] } );
 
-    $('.inner-part')
-      .html(theCompiledHtml)
-      .fadeIn( "slow" )
-    ;
-
+    $('.inner-part').html(theCompiledHtml);
+    TweenMax.fromTo($(".inner-part__title"), 1, {opacity: 0, x: 150, ease: Power3.easeOut}, {opacity: 1, x: 0, ease: Power3.easeOut});
+    TweenMax.fromTo($(".inner-part-column"), 1, {y:  100, opacity: 0}, {y: 0, opacity: 1});
+    TweenMax.fromTo($(".inner-part__image"), 1.5, {rotation: 720, opacity: 0, scale: 0}, {rotation: 0, opacity: 1, scale: 1});
   }
 
 });
